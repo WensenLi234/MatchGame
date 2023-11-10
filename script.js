b0dfdc900b0f326da940f477573d0cd46aa9bc21
unknown <BT_5E7_01@BKTECH.LOCAL>
2023-11-10 15:05:03 -0500

    added win condition

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const resetButton = document.getElementById("reset");
 const guideButton = document.getElementById("guideButton");
-const scoreButton = document.getElementById("scoreButton");
 const guidePara = document.getElementById("guideParagraph");
 const cardContainer = document.getElementById("cards");
-const scorePara = document.getElementById("score");
+const scorePara = document.getElementById("guessParagraph");
 const cards = document.getElementsByClassName("card");
 const images = {
     "circle": "images/Circle.png",
     "square": "images/Square.png",
     "triangle": "images/Triangle.png",
-    "kite": "images/Kite.png"
+    "kite": "images/Kite.png",
+    "trapezoid": "images/Trapezoid.png",
+    "hexagon": "images/Hexagon.png",
+    "star": "images/Star.png",
+    "semicircle": "images/Semicircle",
 }
+const pattern = "Pattern.png";
 let guideOn = true;
 let scoreOn = true;
+let cardsSelected = [];
+let matchedCardCounter = 0;
+let score = 0;
 /* Initializer function:
     Should be called after page loads
 */
@@ -22,8 +29,10 @@ function initialize() {
 // resets game
 function reset() {
     for(let i of cards) {
-        i.src = "Pattern.png";
+        i.src = pattern;
     }
+    score = 0;
+    matchedCardCounter = 0;
 }
 // Maybe combine the two functions?
 function toggleGuide() {
@@ -46,19 +55,49 @@ function toggleScore() {
 }
 
 function revealCard(object) {
+    object.src = getCardShape(object);
+    return object;
+}
+function getCardShape(object) {
     let cardClass = object.className;
     for(let i in images) {
         if(cardClass.search(i) >= 0) {
-            object.src = images[i];
-            return i;
+            return images[i];
         }
     }
 }
+function processGuess(object) {
+    cardsSelected.push(object);
+    if(cardsSelected.length >= 2) {
+        if(getCardShape(cardsSelected[0]) === getCardShape(object)) {
+            matchedCardCounter ++;
+        } else {
+            cardsSelected.forEach(i => {
+                i.src = pattern;
+            });
+        }
+        cardsSelected = [];
+    }
+}
+function displayScore() {
+    if(score == 1) {
+        scorePara.textContent = `You have made ${score} guess.`; 
+    } else {
+        scorePara.textContent = `You have made ${score} guesses.`;
+    }
+} 
 resetButton.addEventListener("click", reset);
 guideButton.addEventListener("click", toggleGuide);
 for(let i of cards) {
     i.addEventListener("click", function(){
-        revealCard(i);
+        if(i.src.toString().search(pattern) >= 0) {
+            score++;
+            processGuess(revealCard(i));
+            displayScore();
+            if(matchedCardCounter >= 8) {
+                scorePara.innerHTML = scorePara.innerHTML + `<br/>You won!`
+            }
+        }
     });
 }
 initialize();
